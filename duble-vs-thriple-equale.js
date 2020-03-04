10b5b287ec1ebbd5274e65155eac1c2e4999029a
hmzsumon <zsumonn@gmail.com>
2020-03-04 08:17:07 +0600

    commit no-4

---
+++ duble-vs-thriple-equale.js
@@ -0,0 +1,21 @@
+const first = 2;
+const second = 3;
+if (first == second) {
+	console.log('condition is true');
+} else {
+	console.log('condition id false');
+}
+const first = 2;
+// const second = 2;
+// if (first == second) {
+// 	console.log('condition is true');
+// } else {
+// 	console.log('condition id false');
+// }
+const first = 2;
+const second = '2';
+if (first === second) {
+	console.log('condition is true');
+} else {
+	console.log('condition id false');
+}
