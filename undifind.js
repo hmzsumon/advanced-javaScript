a95944114876e7302df71d4e7855ecad1ff029e6
hmzsumon <zsumonn@gmail.com>
2020-03-04 07:49:48 +0600

    coomit01

--- undifind.js
+++ undifind.js
@@ -1,6 +1,11 @@
-function add(num1, num2) {
-	let result = num1 + num2;
-	return result;
-}
-let result = add(12, 12);
-console.log(result);
+// function add(num1, num2) {
+// 	let result = num1 + num2;
+// 	return result;
+// }
+// let result = add(12, 12);
+// console.log(result);
+const student = {
+	name: 'Sumon',
+	id: '01757'
+};
+console.log(student.foon);
